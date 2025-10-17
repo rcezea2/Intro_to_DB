28faa405538f51192712efddf444525364537bfa
Richard <rcezea007@gmail.com>
2025-10-17 13:38:46 +0100

    0. A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,34 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-
-CREATE TABLE alx_book_store.Books (
+USE alx_book_store;
+CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES alx_book_store.Authors (author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 );
 
-CREATE TABLE alx_book_store.Authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE alx_book_store.Customers (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE alx_book_store.Orders (
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
-CREATE TABLE alx_book_store.Order_Details (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
@@ -36,4 +36,3 @@ CREATE TABLE alx_book_store.Order_Details (
     FOREIGN KEY (order_id) REFERENCES alx_book_store.Orders (order_id),
     FOREIGN KEY (book_id) REFERENCES alx_book_store.Books (book_id)
 );
-
