65760a4bc602eaaeaa7106379dc7a9f69e19f4c6
Richard <rcezea007@gmail.com>
2025-10-17 18:06:54 +0100

    5. Populating Your Database with Your Very First Data

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- 5. Populating Your Database with Your Very First Data
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
