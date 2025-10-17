b67d239cd16282c03d95aa5970a1026fd83c91bb
Richard <rcezea007@gmail.com>
2025-10-17 13:24:22 +0100

    0. A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ alx_book_store.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE alx_book_store.Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES alx_book_store.Authors (author_id)
+);
+
+CREATE TABLE alx_book_store.Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE alx_book_store.Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE alx_book_store.Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES alx_book_store.Customers (customer_id)
+);
+
+CREATE TABLE alx_book_store.Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES alx_book_store.Orders (order_id),
+    FOREIGN KEY (book_id) REFERENCES alx_book_store.Books (book_id)
+);
+
